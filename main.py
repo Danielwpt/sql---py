596ba903e62dcfe6171f601a54a4da51870ac0ec
Daniel <danielWong151999@gmail.com>
2023-01-28 15:41:33 +0800

    modify connection, select, fetch data in try

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import sqlite3
 
-test_conn = sqlite3.connect("test.db")
+try:
+    test_conn = sqlite3.connect("test.db")
 
-#insert_query = "INSERT INTO People(name, age, address) VALUES ('Adam',12,'Sam 12');"
+    #insert_query = "INSERT INTO People(name, age, address) VALUES ('Adam',12,'Sam 12');"
 
-select_query = "SELECT * FROM People;"
+    test_cur = test_conn.cursor()
 
-test_cur = test_conn.cursor()
+    print("Connected SQLite")
 
-print("Connected SQLite")
+    select_query = "SELECT * FROM People;"
+    test_cur.execute(select_query)
 
-test_cur.execute(select_query)
-
-#test_conn.commit()
-
-fetch_row = test_cur.fetchall()
-
-for rows in fetch_row:
-    print(rows)
-
-test_cur.close()
+    #test_conn.commit()
+    fetch_row = test_cur.fetchall()
+    
+    for rows in fetch_row:
+        print(rows)
+        
+finally:
+    test_cur.close()
